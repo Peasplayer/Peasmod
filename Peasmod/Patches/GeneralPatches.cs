dcc2612a956d084b1d01ec182077f9ffb94635da
Peasplayer <peasplayer@peasplayer.tk>
2021-12-12 12:31:07 +0100

    Implementing the general settings

--- Peasmod/Patches/GeneralPatches.cs
+++ Peasmod/Patches/GeneralPatches.cs
@@ -8,39 +8,31 @@ namespace Peasmod.Patches
 {
     public class GeneralPatches
     {
-        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.ReportClosest))]
-        class ReportBodyButtonPatch
-        {
-            static void Prefix(PlayerControl __instance)
-            {   
-                if(!Settings.ReportBodys.Value || GameModeManager.IsGameModeActive(BattleRoyale.Instance)) //TO-DO: Add potato mode exception
-                {
-                    foreach (var body in Object.FindObjectsOfType<DeadBody>())
-                    {
-                        body.Reported = true;
-                        DestroyableSingleton<ReportButton>.Instance.SetActive(false);
-                    }
-                }
-            }
-        }
-
         [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
         class ActivateVentButtonPatch
         {
             static void Postfix(PlayerControl __instance)
             {
-                if (Settings.CrewVenting.Value && PeasAPI.PeasAPI.GameStarted && !MeetingHud.Instance)
+                var player = PlayerControl.LocalPlayer;
+                
+                if (Settings.CrewVenting.Value && !player.Data.Role.IsImpostor && PeasAPI.PeasAPI.GameStarted && !MeetingHud.Instance)
                 {
                     HudManager.Instance.ImpostorVentButton.gameObject.SetActive(true);
-                    if (__instance.CanMove && !__instance.Data.IsDead)
-                    {
+                    if (player.CanMove && !player.Data.IsDead)
                         HudManager.Instance.ImpostorVentButton.SetEnabled();
-                    }
                     else
-                    {
                         HudManager.Instance.ImpostorVentButton.SetDisabled();
-                    }
                 }
+
+                if (!Settings.Venting.Value && player.Data.Role.IsImpostor && PeasAPI.PeasAPI.GameStarted &&
+                    !MeetingHud.Instance)
+                    HudManager.Instance.ImpostorVentButton.gameObject.SetActive(false);
+                
+                if (!Settings.Sabotaging.Value && PeasAPI.PeasAPI.GameStarted && !MeetingHud.Instance)
+                    HudManager.Instance.SabotageButton.gameObject.SetActive(false);
+                
+                if (!Settings.ReportBodys.Value && PeasAPI.PeasAPI.GameStarted && !MeetingHud.Instance)
+                    HudManager.Instance.ReportButton.gameObject.SetActive(false);
             }
         }
         
@@ -51,7 +43,7 @@ namespace Peasmod.Patches
             public static void Postfix(Vent __instance, [HarmonyArgument(1)] ref bool canUse,
                 [HarmonyArgument(2)] ref bool couldUse, ref float __result)
             {
-                if (Settings.CrewVenting.Value)
+                if (Settings.CrewVenting.Value && !PlayerControl.LocalPlayer.Data.Role.IsImpostor)
                 {
                     couldUse = !PlayerControl.LocalPlayer.Data.IsDead;
                     canUse = couldUse;
@@ -59,63 +51,13 @@ namespace Peasmod.Patches
                     canUse &= distance <= __instance.UsableDistance;
                     __result = distance;
                 }
-            }
-        }
-
-        [HarmonyPatch(typeof(Vent), nameof(Vent.CanUse))]
-        public static class VentCanUsePatch
-        {
-            public static bool Prefix(Vent __instance, ref float __result, [HarmonyArgument(0)] GameData.PlayerInfo pc, [HarmonyArgument(1)] out bool canUse, [HarmonyArgument(2)] out bool couldUse)
-            {
-                canUse = couldUse = false;
-                float num = float.MaxValue;
-                PlayerControl @object = pc.Object;
-                couldUse = ((pc.Role.CanVent && pc.Role.CanUse(__instance.Cast<IUsable>()) || Settings.CrewVenting.Value) && !@object.MustCleanVent(__instance.Id) && !pc.IsDead && (@object.CanMove || @object.inVent));
-                if (ShipStatus.Instance.Systems.ContainsKey(SystemTypes.Ventilation))
+                
+                if (!Settings.Venting.Value && PlayerControl.LocalPlayer.Data.Role.IsImpostor)
                 {
-                    VentilationSystem ventilationSystem = ShipStatus.Instance.Systems[SystemTypes.Ventilation].Cast<VentilationSystem>();
-                    if (ventilationSystem != null && ventilationSystem.IsVentCurrentlyBeingCleaned(__instance.Id))
-                    {
-                        couldUse = false;
-                    }
-                }
-                canUse = couldUse;
-                if (canUse)
-                {
-                    Vector3 center = @object.Collider.bounds.center;
-                    Vector3 position = __instance.transform.position;
-                    num = Vector2.Distance(center, position);
-                    canUse &= (num <= __instance.UsableDistance && !PhysicsHelpers.AnythingBetween(@object.Collider, center, position, Constants.ShipOnlyMask, false));
-                }
-                __result = num;
-                float distance = float.MaxValue;
-                if (!__instance.enabled || !Settings.Venting.Value)
-                {
-                    canUse = false;
-                    couldUse = false;
-                    return false;
-                }
-                PlayerControl localPlayer = pc.Object;
-                if (localPlayer.Data.Role.IsImpostor)
-                {
-                    couldUse = !localPlayer.Data.IsDead;
-                    canUse = couldUse;
-                    distance = Vector2.Distance(localPlayer.GetTruePosition(), __instance.transform.position);
-                    canUse &= distance <= __instance.UsableDistance;
+                    var distance = Vector2.Distance(PlayerControl.LocalPlayer.GetTruePosition(), __instance.transform.position);
+                    canUse = couldUse = false;
                     __result = distance;
                 }
-                else
-                {
-                    if (Settings.CrewVenting.Value)
-                    {
-                        couldUse = !localPlayer.Data.IsDead;
-                        canUse = couldUse;
-                        distance = Vector2.Distance(localPlayer.GetTruePosition(), __instance.transform.position);
-                        canUse &= distance <= __instance.UsableDistance;
-                        __result = distance;
-                    }
-                }
-                return false;
             }
         }
         
